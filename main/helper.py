e1442b7224e251fc16480f531f63e644276d12bd
felekekinfe <felekekinfe@gmail.com>
2025-04-07 20:34:18 +0300

    commit

--- main/helper.py
+++ main/helper.py
@@ -44,7 +44,7 @@
 #                     'G': '6',
 #                     'S': '5'}
 
-# dict_int_to_char = {'0': 'O',
+# dict_char_to_int = {'0': 'O',
 #                     '1': 'I',
 #                     '3': 'J',
 #                     '4': 'A',
@@ -88,7 +88,7 @@
 #                                                             results[frame_nmr][car_id]['license_plate']['text_score'])
 #                             )
 #         f.close()
-import string
+
 import easyocr
 
 # Initialize the OCR reader
@@ -102,50 +102,60 @@ dict_char_to_int = {'O': '0',
                     'G': '6',
                     'S': '5'}
 
-dict_int_to_char = {'0': 'O',
-                    '1': 'I',
-                    '3': 'J',
-                    '4': 'A',
-                    '6': 'G',
-                    '5': 'S'}
+# dict_char_to_int = {'0': 'O',
+#                     '1': 'I',
+#                     '3': 'J',
+#                     '4': 'A',
+#                     '6': 'G',
+#                     '5': 'S'}
 
 
-def write_csv(results, output_path):
+
+def license_complies_format(text):
     """
-    Write the results to a CSV file.
+    Check if the license plate text complies with the required format.
 
     Args:
-        results (dict): Dictionary containing the results.
-        output_path (str): Path to the output CSV file.
-    """
-    with open(output_path, 'w') as f:
-        f.write('{},{},{},{},{},{},{}\n'.format('frame_nmr', 'car_id', 'car_bbox',
-                                                'license_plate_bbox', 'license_plate_bbox_score', 'license_number',
-                                                'license_number_score'))
+        text (str): License plate text.
 
-        for frame_nmr in results.keys():
-            for car_id in results[frame_nmr].keys():
-                print(results[frame_nmr][car_id])
-                if 'car' in results[frame_nmr][car_id].keys() and \
-                   'license_plate' in results[frame_nmr][car_id].keys() and \
-                   'text' in results[frame_nmr][car_id]['license_plate'].keys():
-                    f.write('{},{},{},{},{},{},{}\n'.format(frame_nmr,
-                                                            car_id,
-                                                            '[{} {} {} {}]'.format(
-                                                                results[frame_nmr][car_id]['car']['bbox'][0],
-                                                                results[frame_nmr][car_id]['car']['bbox'][1],
-                                                                results[frame_nmr][car_id]['car']['bbox'][2],
-                                                                results[frame_nmr][car_id]['car']['bbox'][3]),
-                                                            '[{} {} {} {}]'.format(
-                                                                results[frame_nmr][car_id]['license_plate']['bbox'][0],
-                                                                results[frame_nmr][car_id]['license_plate']['bbox'][1],
-                                                                results[frame_nmr][car_id]['license_plate']['bbox'][2],
-                                                                results[frame_nmr][car_id]['license_plate']['bbox'][3]),
-                                                            results[frame_nmr][car_id]['license_plate']['bbox_score'],
-                                                            results[frame_nmr][car_id]['license_plate']['text'],
-                                                            results[frame_nmr][car_id]['license_plate']['text_score'])
-                            )
-        f.close()
+    Returns:
+        bool: True if the license plate complies with the format, False otherwise.
+    """
+    if len(text) != 6:
+        return False
+####ETHIOPIAN TARGA NUMBER LNGTH IS 6***************************************************************
+    if (text[0] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[0] in dict_char_to_int.keys()) and \
+       (text[1] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[1] in dict_char_to_int.keys()) and \
+       (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+       (text[4] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[4] in dict_char_to_int.keys()) and \
+       (text[5] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[5] in dict_char_to_int.keys()):
+       #(text[6] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[6] in dict_char_to_int.keys()):
+        return True
+    else:
+        return False
+
+
+def format_license(text):
+    """
+    Format the license plate text by converting characters using the mapping dictionaries.
+
+    Args:
+        text (str): License plate text.
+
+    Returns:
+        str: Formatted license plate text.
+    """
+    license_plate_ = ''
+    alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    
+    for j in range(6):####ETHIOPIAN TARGA NUMBER LNGTH IS 6***************************************************************
+        if text[j] in alphabet:
+            license_plate_ += dict_char_to_int[text[j]]
+        else:
+            license_plate_ += text[j]
+
+    return license_plate_
 
 
 def read_license_plate(license_plate_crop):
@@ -159,6 +169,18 @@ def read_license_plate(license_plate_crop):
         tuple: Tuple containing the formatted license plate text and its confidence score.
     """
 
+    detections = reader.readtext(license_plate_crop)
+
+    for detection in detections:
+        bbox, text, score = detection
+
+        text = text.upper().replace(' ', '')
+        
+
+        if license_complies_format(text):
+            return format_license(text), score
+
+    return None, None
 
 
 def get_car(license_plate, vehicle_track_ids):
